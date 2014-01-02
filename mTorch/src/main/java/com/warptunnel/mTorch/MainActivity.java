240dfacd944262cabf3084c42a2f5aa05134c9bf
wKovacs64 <justin.r.hall@gmail.com>
2014-01-01 21:05:57 -0700

    Add debugging, begin accessing UI elements

--- mTorch/src/main/java/com/warptunnel/mTorch/MainActivity.java
+++ mTorch/src/main/java/com/warptunnel/mTorch/MainActivity.java
@@ -1,17 +1,19 @@
 package com.warptunnel.mTorch;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.ImageButton;
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +26,37 @@ public class MainActivity extends ActionBarActivity {
                     .commit();
         }
     }
+/*
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "********** onStart **********");
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "********** onResume **********");
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "********** onPause **********");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "********** onStop **********");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "********** onDestroy **********");
+    }
+*/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         
@@ -53,7 +84,10 @@ public class MainActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class PlaceholderFragment extends Fragment implements View.OnClickListener {
+
+        private static final String TAG = PlaceholderFragment.class.getSimpleName();
+        private ImageButton mImageButton;
 
         public PlaceholderFragment() {
         }
@@ -64,6 +98,51 @@ public class MainActivity extends ActionBarActivity {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            Log.d(TAG, "********** onStart **********");
+
+            mImageButton = (ImageButton) this.getActivity().findViewById(R.id.torch_imagebutton);
+            if (mImageButton == null) Log.e(TAG, "mImageButton was NULL");
+            else {
+                //mImageButton.setImageResource(R.drawable.torch_off);
+                mImageButton.setOnClickListener(this);
+                mImageButton.setEnabled(false);
+            }
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            Log.d(TAG, "********** onResume **********");
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            Log.d(TAG, "********** onPause **********");
+        }
+
+        @Override
+        public void onStop() {
+            super.onStop();
+            Log.d(TAG, "********** onStop **********");
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            Log.d(TAG, "********** onDestroy **********");
+        }
+
+        @Override
+        public void onClick(View v) {
+            Log.d(TAG, "********** onClick **********");
+            // Toggle torch here
+        }
+
     }
 
 }
