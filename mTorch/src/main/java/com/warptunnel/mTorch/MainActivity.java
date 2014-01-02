9244b46dd6bb680b49d7e53cb87a5e9c7a59ae50
wKovacs64 <justin.r.hall@gmail.com>
2014-01-02 13:57:48 -0700

    Remove usued MainActivity debugging

--- mTorch/src/main/java/com/warptunnel/mTorch/MainActivity.java
+++ mTorch/src/main/java/com/warptunnel/mTorch/MainActivity.java
@@ -26,37 +26,7 @@ public class MainActivity extends ActionBarActivity {
                     .commit();
         }
     }
-/*
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "********** onStart **********");
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "********** onResume **********");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "********** onPause **********");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "********** onStop **********");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "********** onDestroy **********");
-    }
-*/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
