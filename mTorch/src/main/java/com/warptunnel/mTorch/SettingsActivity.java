9df7f9ea425f9088dfb30f9c156dcbc144e1f9ef
wKovacs64 <justin.r.hall@gmail.com>
2014-01-11 13:16:11 -0700

    Settings: logging for debugging
    Also, insignificantly updated version in build.gradle

--- mTorch/src/main/java/com/warptunnel/mTorch/SettingsActivity.java
+++ mTorch/src/main/java/com/warptunnel/mTorch/SettingsActivity.java
@@ -2,15 +2,19 @@ package com.warptunnel.mTorch;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.util.Log;
 
 public class SettingsActivity extends PreferenceActivity {
+
+    private static final String TAG = SettingsActivity.class.getSimpleName();
 
     @SuppressWarnings("deprecation")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitle(getString(R.string.app_name) + " " + getString(R.string.menu_settings));
+        Log.d(TAG, "********** Settings **********");
 
+        setTitle(getString(R.string.app_name) + " " + getString(R.string.menu_settings));
         addPreferencesFromResource(R.xml.settings);
     }
 }
