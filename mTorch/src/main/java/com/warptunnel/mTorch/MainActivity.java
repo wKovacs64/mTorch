b6fb569dd8bd46f80c02739cac0ffad014698e92
wKovacs64 <justin.r.hall@gmail.com>
2014-01-13 20:54:39 -0700

    Remove MainFragment and collapse content into parent MainActivity
    A Fragment adds no value to this particular app as it really only has one view
    (plus the PreferenceActivity).
    
    Note: git detected renames on the layout files because the content was moved
    from fragment_main to activity_main so the similarity index was extremely high.
    In actual fact, the fragment layout files were deleted and the activity layout
    files were updated.

--- mTorch/src/main/java/com/warptunnel/mTorch/MainActivity.java
+++ mTorch/src/main/java/com/warptunnel/mTorch/MainActivity.java
@@ -6,42 +6,107 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-
     private AboutDialog mAboutDialog;
+    private Context mContext;
+    private ImageButton mImageButton;
+    private boolean mTorchEnabled;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        Log.d(TAG, "********** onCreate **********");
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new MainFragment())
-                    .commit();
-        }
 
+        // Assume flash off on launch (certainly true the first time)
+        mTorchEnabled = false;
+
+        // Set up the About dialog box
         mAboutDialog = new AboutDialog(this);
+
+        // Track this Context for potentially passing it around later
+        mContext = getApplicationContext();
+
+        // Check for flash capability
+        if (!mContext.getPackageManager().hasSystemFeature(PackageManager
+                .FEATURE_CAMERA_FLASH)) {
+            Log.e(TAG, getString(R.string.error_no_flash));
+            Toast.makeText(mContext, R.string.error_no_flash, Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        Log.d(TAG, getString(R.string.debug_flash_found));
+
+        // Set up the clickable toggle image
+        mImageButton = (ImageButton) findViewById(R.id.torch_image_button);
+        if (mImageButton == null) Log.e(TAG, "mImageButton was NULL");
+        else {
+            mImageButton.setImageResource(R.drawable.torch_off);
+            mImageButton.setOnClickListener(this);
+            mImageButton.setEnabled(true);
+
+            // Keep the screen on while the app is open
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "********** onStart **********");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "********** onResume **********");
+
+        mTorchEnabled = mTorchService.isRunning();
+        updateImageButton();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "********** onPause **********");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "********** onStop **********");
+
+        // Close the About dialog if we're stopping anyway
+        if (mAboutDialog.isShowing()) mAboutDialog.dismiss();
+
+        // Check if the user enabled persistence and stop the service if not
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(mContext);
+        boolean persist = sharedPref.getBoolean(getString(R.string.persistence), false);
+        if (!persist) mContext.stopService(new Intent(mContext, mTorchService.class));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "********** onDestroy **********");
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -66,139 +131,38 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
+    public void onClick(View v) {
+        Log.d(TAG, "********** onClick **********");
 
-        if (mAboutDialog.isShowing()) mAboutDialog.dismiss();
+        toggleTorch();
     }
 
-    public static class MainFragment extends Fragment implements View.OnClickListener {
+    private void toggleTorch() {
+        Log.d(TAG, "toggleTorch | mTorchEnabled was " + mTorchEnabled + " when image was " +
+                "pressed; changing to " + !mTorchEnabled);
 
-        private static final String TAG = MainFragment.class.getSimpleName();
-        private ImageButton mImageButton;
-        private Context mContext;
-        private FragmentActivity mActivity;
-        private boolean mTorchEnabled;
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(mContext);
+        boolean persist = sharedPref.getBoolean(getString(R.string.persistence), false);
 
-        public MainFragment() {
-            mTorchEnabled = false;
+        // Use the service to start/stop the torch (start = on, stop = off)
+        if (mTorchEnabled) {
+            mContext.stopService(new Intent(mContext, mTorchService.class));
+        }
+        else {
+            mContext.startService(new Intent(mContext, mTorchService.class).putExtra
+                    (getString(R.string.persistence), persist));
         }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            Log.d(TAG, "********** onCreateView **********");
+        mTorchEnabled = !mTorchEnabled;
+        updateImageButton();
+    }
 
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
+    private void updateImageButton() {
+        Log.d(TAG, "updateImageButton | mTorchEnabled = " + mTorchEnabled + "; setting image " +
+                "accordingly");
 
-        @SuppressWarnings("deprecation")
-        @Override
-        public void onStart() {
-            super.onStart();
-            Log.d(TAG, "********** onStart **********");
-
-            // Obtain Activity and Context for communicating with UI later
-            mActivity = getActivity();
-            mContext = mActivity.getApplicationContext();
-
-            // Check for flash capability
-            if (!mContext.getPackageManager().hasSystemFeature(PackageManager
-                    .FEATURE_CAMERA_FLASH)) {
-                Log.e(TAG, getString(R.string.error_no_flash));
-                Toast.makeText(mContext, R.string.error_no_flash, Toast.LENGTH_LONG).show();
-                mActivity.finish();
-                return;
-            }
-
-            Log.d(TAG, getString(R.string.debug_flash_found));
-
-            // Set up the clickable toggle image
-            mImageButton = (ImageButton) mActivity.findViewById(R.id.torch_image_button);
-            if (mImageButton == null) Log.e(TAG, "mImageButton was NULL");
-            else {
-                mImageButton.setImageResource(R.drawable.torch_off);
-                mImageButton.setOnClickListener(this);
-                mImageButton.setEnabled(false);
-
-                // Allow the toggle image to be clicked
-                mImageButton.setEnabled(true);
-
-                // Keep the screen on while the app is open
-                mActivity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-            }
-        }
-
-        @Override
-        public void onResume() {
-            super.onResume();
-            Log.d(TAG, "********** onResume **********");
-
-            mTorchEnabled = mTorchService.isRunning();
-            updateImageButton();
-        }
-
-        @Override
-        public void onPause() {
-            super.onPause();
-            Log.d(TAG, "********** onPause **********");
-
-        }
-
-        @Override
-        public void onStop() {
-            super.onStop();
-            Log.d(TAG, "********** onStop **********");
-
-            SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(mContext);
-            boolean persist = sharedPref.getBoolean(getString(R.string.persistence), false);
-
-            if (!persist) mContext.stopService(new Intent(mContext, mTorchService.class));
-        }
-
-        @Override
-        public void onDestroy() {
-            super.onDestroy();
-            Log.d(TAG, "********** onDestroy **********");
-
-        }
-
-        @Override
-        public void onClick(View v) {
-            Log.d(TAG, "********** onClick **********");
-
-            toggleTorch();
-        }
-
-        private void toggleTorch() {
-            Log.d(TAG, "toggleTorch | mTorchEnabled was " + mTorchEnabled + " when image was " +
-                    "pressed; changing to " + !mTorchEnabled);
-
-            SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(mContext);
-            boolean persist = sharedPref.getBoolean(getString(R.string.persistence), false);
-
-            // Use the service to start/stop the torch (start = on, stop = off)
-            if (mTorchEnabled) {
-                mContext.stopService(new Intent(mContext, mTorchService.class));
-            }
-            else {
-                mContext.startService(new Intent(mContext, mTorchService.class).putExtra
-                        (getString(R.string.persistence), persist));
-            }
-
-            mTorchEnabled = !mTorchEnabled;
-            updateImageButton();
-        }
-
-        private void updateImageButton() {
-            Log.d(TAG, "updateImageButton | mTorchEnabled = " + mTorchEnabled + "; setting image " +
-                    "accordingly");
-
-            if (mTorchEnabled) mImageButton.setImageResource(R.drawable.torch_on);
-            else mImageButton.setImageResource(R.drawable.torch_off);
-        }
-
+        if (mTorchEnabled) mImageButton.setImageResource(R.drawable.torch_on);
+        else mImageButton.setImageResource(R.drawable.torch_off);
     }
 
 }
